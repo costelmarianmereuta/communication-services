fbca58f88cb56008eb00ef90eca3eeae0bfc9443
costelmarianmereuta <mereuta.mariana1@gmail.com>
2021-03-10 08:16:11 +0100

    first commit example

--- testms/src/main/java/com/example/testms/Controller.java
+++ testms/src/main/java/com/example/testms/Controller.java
@@ -1,21 +1,17 @@
-package org.sid.servicecompany.controller;
+package com.example.testms;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 public class Controller {
 
-    @Value("${yp}")
-    private int yparam;
-    @Value("${name}")
+    @Value("${test}")
     private String name;
 
     @GetMapping("/test")
     public String test(){
-        return String.valueOf(yparam);
+        return name;
     }
 }
---
+++ testms/src/test/java/com/example/testms/TestmsApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.testms;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TestmsApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
